994af5ef8c8a69a244a9b81805ea3cc2d42ff247
kubashh <146523311+kubashh@users.noreply.github.com>
2025-03-20 18:02:06 +0000

    0.0.0.1

---
+++ frontend/src/App.tsx
@@ -0,0 +1,8 @@
+export const App = () => {
+  return (
+    <>
+      <h1>Freedom App</h1>
+      <input type="button" value="test" />
+    </>
+  )
+}
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,9 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import "./index.css"
-import { App } from "./App"
+import { App } from "./App.tsx"
 
-const root = createRoot(document.getElementById(`root`))
-root.render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>
